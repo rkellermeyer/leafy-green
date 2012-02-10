29467b409decd338c9b4516609f523a4c3345398
Richard Kellermeyer <rnkconcepts@gmail.com>
2012-02-09 21:15:14 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ratemy::Application.config.secret_token = '92ed95de8ac86141578a4c8c99916a79edeed014c87f13cc623988ef5e946be1d44565c92c65f127d1a5a81bbd417561bd1c292e9ef2f07ee748a0f4389818e5'
